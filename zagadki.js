fa5795cb40eee978c287a1c9110a480a4bc2c0ac
Maciej <64641956+Niutaq@users.noreply.github.com>
2021-04-20 21:19:50 +0200

    Add files via upload

---
+++ zagadki.js
@@ -0,0 +1,100 @@
+function sprawdz()
+    {
+        document.getElementById('animate__show').innerHTML= '<span style="font-size:15px"> 12 (po 4 minutach 6 uczniów rozwiąże 6 zadań, po 8 minutach to będzie 12 zadań, a po 12 minutach 18 zadań). </span>'; 
+        document.getElementById('animate__show').style.color='blueviolet';
+        document.getElementById('animate__show').style.fontFamily= 'bellota';
+        document.getElementById('animate__show').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show').style.textAlign= 'center';
+    }
+            function ukryj()
+            {
+                document.getElementById("animate__show").innerHTML = '<span style="font-size:30px"> Kliknij poniżej, aby zobaczyć odpowiedź. </span>';
+                document.getElementById('animate__show').style.color='blueviolet';
+                document.getElementById('animate__show').style.fontFamily= 'bellota';
+                document.getElementById('animate__show').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show').style.textAlign= 'center';
+            }   
+
+
+            
+function sprawdz2()
+    {
+                document.getElementById('animate__show2').innerHTML= '<span style="font-size:15px"> 22 (W skrajnym przypadku najpierw wyciągniemy wszystkie kule zielone i niebieskie, a na końcu 2 żółte, w sumie 22). </span>'; 
+                document.getElementById('animate__show2').style.color='blueviolet';
+                document.getElementById('animate__show2').style.fontFamily= 'bellota';
+                document.getElementById('animate__show2').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show2').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show2').style.textAlign= 'center';
+     }
+            function ukryj2()
+            {
+                document.getElementById("animate__show2").innerHTML = '<span style="font-size:30px"> Kliknij poniżej, aby zobaczyć odpowiedź. </span>';
+                document.getElementById('animate__show2').style.color='blueviolet';
+                document.getElementById('animate__show2').style.fontFamily= 'bellota';
+                document.getElementById('animate__show2').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show2').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show2').style.textAlign= 'center';
+            }   
+
+function sprawdz3()
+    {
+        document.getElementById("animate__show3").innerHTML= '<span style="font-size:35px"> 13 (Jedna kreska znaku = 1 ). </span>'; 
+        document.getElementById('animate__show3').style.color='blueviolet';
+        document.getElementById('animate__show3').style.fontFamily= 'bellota';
+        document.getElementById('animate__show3').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show3').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show3').style.textAlign= 'center';
+    }
+
+            function ukryj3()
+            {
+                document.getElementById("animate__show3").innerHTML = '<span style="font-size:30px"> Kliknij poniżej, aby zobaczyć odpowiedź. </span>';
+                document.getElementById('animate__show3').style.color='blueviolet';
+                document.getElementById('animate__show3').style.fontFamily= 'bellota';
+                document.getElementById('animate__show3').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show3').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show3').style.textAlign= 'center';
+            }   
+
+function sprawdz4()
+    {
+        document.getElementById("animate__show4").innerHTML= '<span style="font-size:25px"> Mężczyzna, który prosił o wodę, miał czkawkę i kelner postanowił, że go przestraszy, żeby mu pomóc. </span>'; 
+        document.getElementById('animate__show4').style.color='blueviolet';
+        document.getElementById('animate__show4').style.fontFamily= 'bellota';
+        document.getElementById('animate__show4').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show4').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show4').style.textAlign= 'center';
+    }
+            function ukryj4()
+            {
+                document.getElementById("animate__show4").innerHTML = '<span style="font-size:30px"> Kliknij poniżej, aby zobaczyć odpowiedź. </span>';
+                document.getElementById('animate__show4').style.color='blueviolet';
+                document.getElementById('animate__show4').style.fontFamily= 'bellota';
+                document.getElementById('animate__show4').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show4').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show4').style.textAlign= 'center';
+            }   
+
+function sprawdz5()
+    {
+        document.getElementById('animate__show5').innerHTML= '<span style="font-size:27px"> Mężczyzna był latarnikiem. Przez niego statek rozbił się o skały. Zginęło 90 osób. </span>'; 
+        document.getElementById('animate__show5').style.color='blueviolet';
+        document.getElementById('animate__show5').style.fontFamily= 'bellota';
+        document.getElementById('animate__show5').style.animationName= 'jackInTheBox';
+        document.getElementById('animate__show5').style.animationTimingFunction= 'ease-in-out'
+        document.getElementById('animate__show5').style.textAlign= 'center';
+        document.getElementById('animate__show5').style.marginBottom= '20px';
+    }
+
+            function ukryj5()
+            {
+                document.getElementById('animate__show5').innerHTML = '<span style="font-size:30px"> Kliknij poniżej, aby zobaczyć odpowiedź. </span>';
+                document.getElementById('animate__show5').style.color='blueviolet';
+                document.getElementById('animate__show5').style.fontFamily= 'bellota';
+                document.getElementById('animate__show5').style.animationName= 'jackInTheBox';
+                document.getElementById('animate__show5').style.animationTimingFunction= 'ease-in-out'
+                document.getElementById('animate__show5').style.textAlign= 'center';
+                document.getElementById('animate__show5').style.marginBottom= '20px';
+            } 
